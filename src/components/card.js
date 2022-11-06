468fa00412643c5e75296beb8dc2c63b63ba2f20
LinFury <vladikamir.dev@gmail.com>
2022-11-06 12:20:08 +0500

    Спринт 7

---
+++ src/components/card.js
@@ -0,0 +1,70 @@
+const photoCards = document.querySelector(".photo__cards");
+const cardTemplate = document.querySelector("#card").content;
+
+const initialCards = [
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
+
+export function renderInitialCards() {
+  initialCards.forEach((element) => {
+    const card = createCard(element.name, element.link);
+    photoCards.append(card);
+  });
+}
+
+export function addCard(name, link) {
+  const card = createCard(name, link);
+  photoCards.prepend(card);
+}
+
+function createCard(name, link) {
+  const card = cardTemplate.querySelector(".photo__card").cloneNode(true);
+  const photoImage = card.querySelector(".photo__image");
+  photoImage.src = link;
+  photoImage.alt = name;
+
+  card.querySelector(".photo__title").textContent = name;
+  card
+    .querySelector(".photo__like")
+    .addEventListener("click", function (event) {
+      event.target.classList.toggle("photo__like_active");
+    });
+  card
+    .querySelector(".photo__delete")
+    .addEventListener("click", function (event) {
+      event.target.closest(".photo__card").remove();
+    });
+
+  photoImage.addEventListener("click", function (event) {
+    popupImg.src = link;
+    popupImg.alt = name;
+    popupImgTitle.textContent = name;
+
+    openPopup(popupImage);
+  });
+
+  return card;
+}
